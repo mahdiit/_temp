feec3eb54673f052e8b88547970a26afeffc6763
mahdiit <67848+mahdiit@users.noreply.github.com>
2019-01-22 15:20:08 +0330

    add first page date

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 $(function() {
+    moment.loadPersian({ usePersianDigits: true });
     $('.fixdate').each(function() {
         var m = $(this).attr('data-date');
-        $(this).html(moment(m, "YYYY-M-D").format('jYYYY/jM/jD'));
+        $(this).html(moment(m, "YYYY-M-D").format('dddd, jD MMMM jYYYY'));
     });
 });
