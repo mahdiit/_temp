a7a2a14e1a887797c18d24c7e48067bfd442696a
mahdiit <67848+mahdiit@users.noreply.github.com>
2019-01-22 15:27:31 +0330

    fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,7 @@
 $(function() {
     moment.loadPersian({ usePersianDigits: true });
     $('.fixdate').each(function() {
-        var m = $(this).attr('data-date');
+        var m = $(this).attr('datetime');
         $(this).html(moment(m, "YYYY-M-D").format('dddd, jD jMMMM jYYYY'));
     });
 });
