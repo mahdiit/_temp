12e9a6334609c953df325fadaa75f8e849ea7b9a
Mahdi Yousefi <67848+mahdiit@users.noreply.github.com>
2019-01-31 19:09:08 +0330

    page slider

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -4,4 +4,25 @@ $(function() {
         var m = $(this).attr('datetime');
         $(this).html(moment(m, "YYYY-M-D").format('dddd, jD jMMMM jYYYY'));
     });
+
+
+    $(".owl-carousel").owlCarousel({
+      items: 5,
+      loop: true,
+      nav : true,
+      rtl: true,
+      navText: ['<i class="fas fa-chevron-circle-right slider-btn"></i>',
+    '<i class="fas fa-chevron-circle-left slider-btn"></i>'],
+      responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+    });
 });
